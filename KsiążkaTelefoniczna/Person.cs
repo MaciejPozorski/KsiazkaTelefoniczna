a0d2c9590bf6738d1a1da79119e78dc8e723fd24
MaciejPozorski <mpozorski01@gmailcom>
2022-02-03 04:15:43 +0100

    Dodaj pliki projektów.

---
+++ KsiążkaTelefoniczna/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KsiążkaTelefoniczna
+{
+    public class Person
+    {
+        private string name;
+        private int number;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Number
+        {
+            get { return number; }
+            set 
+            {
+                if (value.ToString().Length == 9 || value.ToString().Length == 11)
+                {
+                number = value; 
+                }
+                else
+                {
+                Console.WriteLine("invalid value");
+                }
+            }
+        }
+
+        public Person(string name, int number)
+        {
+            Name = name;
+            Number = number;
+        }
+        
+    }
+}
---
+++ KsiążkaTelefoniczna/Program.cs
@@ -0,0 +1,120 @@
+﻿using KsiążkaTelefoniczna;
+
+
+string userInput;
+string userSearchInput;
+static List<Person> Getpeople()
+{
+    List<Person> people = new List<Person>()
+    {
+        new Person("Maciej", 123456789),
+        new Person("Jan", 987654321),
+        new Person("Adam", 789456123),
+        new Person("zbychu", 481234567)
+    };
+    return people;
+}
+
+List<Person> people = Getpeople();
+
+
+  void ShowAll()
+  {
+    foreach (Person person in people)
+    {
+        Console.WriteLine($"Name: {person.Name}, number: {person.Number}");
+        
+    }
+  }
+
+do
+{
+    Console.WriteLine("===================================");
+    Console.WriteLine("| Wybierz:                        |");
+    Console.WriteLine("| 1. Dodaj kontakt                |");
+    Console.WriteLine("| 2. Wyszukaj kontakt             |");
+    Console.WriteLine("| 3. Wyświetl wszystkie kontakty  |");
+    Console.WriteLine("| 0. Wyjście                      |");
+    Console.WriteLine("===================================");
+
+    userInput = Console.ReadLine();
+
+
+    switch (userInput)
+    {
+        case "1":
+            {
+                Console.WriteLine("Podaj imie: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Podaj numer");
+                int nr = int.Parse(Console.ReadLine()); 
+                people.Add(new Person(name, nr));
+                Console.WriteLine("ok");
+                Console.ReadLine();
+                break;
+            }
+        case "2":
+            {
+                Console.WriteLine("Podaj szczegóły: ");
+                userSearchInput = Console.ReadLine();
+                if (int.TryParse(userSearchInput, out int x))
+                {
+                    bool wanted (Person person)
+                    {
+                        return person.Number == x;
+                    }
+
+                    Person wantedPerson = people.FirstOrDefault(wanted);
+                    if (wantedPerson != null)
+                    {
+                        Console.WriteLine($"Name: {wantedPerson.Name}, number: {wantedPerson.Number}");
+                        Console.ReadLine ();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Person not found");
+                        Console.ReadLine ();
+                    }
+                }
+                else
+                {
+                    bool wanted(Person person)
+                    {
+                        return person.Name == userSearchInput;
+                    }
+
+                    Person wantedPerson = people.FirstOrDefault(wanted);
+                    if (wantedPerson != null)
+                    {
+                        Console.WriteLine($"Name: {wantedPerson.Name}, number: {wantedPerson.Number}");
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Person not found");
+                        Console.ReadLine();
+                    }
+                }
+                break;
+            }
+        case "3":
+            {
+                ShowAll();
+                Console.WriteLine("Wciśnij dowolny klawisz aby wyjść");
+                Console.ReadLine();
+                break;
+            }
+        case "0":
+            {
+                break;
+            }
+
+        default:
+            {
+                Console.WriteLine("Zły numer");
+                break;
+            }
+
+    }
+    Console.Clear();
+} while (userInput != "0");
